382cd8cb0d2ec3446b46077cc2665ddaf42ca2ee
sonamk17591 <sonamk17591@gmail.com>
2021-09-22 19:55:09 +0530

    commit

--- enroll/signals.py
+++ enroll/signals.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.dispatch import receiver
 
 
+#SIGNAL FOR USER LOGIN
 
 @receiver (user_logged_in,sender=User)
 def login_success(sender,request,user,**kwargs):
@@ -14,4 +15,34 @@ def login_success(sender,request,user,**kwargs):
     print('User',user.password)
     print(f'Kwargs:{kwargs}')
 #user_logged_in.connect(login_success,sender=User) 
+
+#SIGNAL FOR USER LOG OUT AND LOGIN FAILED
+
+@receiver (user_logged_out,sender=User)
+def logout_success(sender,request,user,**kwargs):
+    print('-------')
+    print('This is logout success signal')
+    print('Sender',sender)
+    print('Request',request)
+    print('User',user)
+    print(f'Kwargs:{kwargs}')
+    
+#user_logged_out.connect(logout_success,sender=User)
+
+#SIGNAL FOR USER LOGIN FAILED 
+
+
+@receiver (user_login_failed)
+def login_fail(sender,request,credentials,**kwargs):
+    print('-------')
+    print('This is login failed signal')
+    print('Sender',sender)
+    print('Request',request)
+    print('credentials',credentials)
+    print(f'Kwargs:{kwargs}')
+#user_logged_out.connect(logout_success,sender=User)    
+
+
+
+
 
